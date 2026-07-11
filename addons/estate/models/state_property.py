9f3fd8cceebb39227c2fc37f6bb8be246951d963
hemany404 <emanuelngoma42@outlook.pt>
2026-07-11 21:59:02 +0100

    upt

---
+++ addons/estate/models/state_property.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class TestModel(models.Models):
+    _name = 'test_model'
+    _description = 'Test Model'
+
+    _order = "sequence"
+
+    name = fields.Char('Plan Name', required=True, translate=True)
+    number_of_months = fields.Integer('# Months', required=True)
+    active = fields.Boolean('Active', default=True)
+    sequence = fields.Integer('Sequence', default=10)
+
+    _sql_constraints = [
+        ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
+    ]
