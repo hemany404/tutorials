f7b46dbe620b6c1983588d7fb6173f19981a5192
hemany404 <emanuelngoma42@outlook.pt>
2026-07-13 15:54:09 +0100

    FEAT: complete de model

--- addons/estate/models/state_property.py
+++ addons/estate/models/state_property.py
@@ -4,12 +4,20 @@ from odoo import fields, models
 class TestModel(models.Model):
     _name = 'test_model'
     _description = 'Test Model'
-
     _order = "sequence"
 
-    name = fields.Char('Name', required=True, translate=True)
-    description = fields.char('Description', required=False, translate=True)
-    price = fields.Integer('price', required=True)
-    
+    nome = fields.Char('Nome', required=True, translate=True)
+    descricao = fields.char('Descricao', required=False)
+    codigo_postal = fields.char('codigo_posta', required=False)
+    data_disponivel = fields.date('data_disponivel', required=False)
+    preco_esperado = fields.float('preco_esperado',required=True)
+    preco_venda = fields.float('Preco_venda', required=True)
+    quartos = fields.integer('quartos', required=True)
+    area_estar = fields.integer('area_estar', required=True)
+    fachadas = fields.integer('fachadas', required=True)
+    garagem = fields.float('garagem', required=True)
+    jardim = fields.float('jardim', required=False)
+    jardim_area = fields.integer('jardim_area', required=True)
+    jardim_orientacao = fields.char('jardim_orientacao',required=False)
 
 
