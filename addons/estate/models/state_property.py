ccf9475ec10d9f0a977bbabfe48f34bd6462c8ae
hemany404 <emanuelngoma42@outlook.pt>
2026-07-13 15:13:04 +0100

    FEAR: create new model imobiliary

--- addons/estate/models/state_property.py
+++ addons/estate/models/state_property.py
@@ -1,17 +1,15 @@
 from odoo import fields, models
 
 
-class TestModel(models.Models):
+class TestModel(models.Model):
     _name = 'test_model'
     _description = 'Test Model'
 
     _order = "sequence"
 
-    name = fields.Char('Plan Name', required=True, translate=True)
-    number_of_months = fields.Integer('# Months', required=True)
-    active = fields.Boolean('Active', default=True)
-    sequence = fields.Integer('Sequence', default=10)
+    name = fields.Char('Name', required=True, translate=True)
+    description = fields.char('Description', required=False, translate=True)
+    price = fields.Integer('price', required=True)
+    
 
-    _sql_constraints = [
-        ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
-    ]
+
