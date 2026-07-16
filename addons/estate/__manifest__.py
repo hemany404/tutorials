333f94714a66c6dc4609c40d40c62ab2675a570e
hemany404 <emanuelngoma42@outlook.pt>
2026-07-16 15:52:04 +0100

    UPT

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -1,14 +1,6 @@
 {
     'name': "estate",
 
-    'summary': """
-        Starting module for "Master the Odoo web framework, chapter 1: Build a Clicker game"
-    """,
-
-    'description': """
-        Starting module for "Master the Odoo web framework, chapter 1: Build a Clicker game"
-    """,
-
 
     'category': 'Teste',
     'version': '0.1',
@@ -16,7 +8,9 @@
     'installable': True,
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menu.xml'
     ],
 
   
--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,19 +1,22 @@
 from odoo import fields, models
+from datetime import timedelta, date
 
 
 class TestModel(models.Model):
     _name = 'test_model'
     _description = 'Test Model'
-    _order = "sequence"
 
-    nome = fields.Char('Nome', required=True, translate=True)
-    descricao = fields.Char('Descricao', required=False)
-    codigo_postal = fields.Char('codigo_posta', required=False)
-    data_disponivel = fields.Date('data_disponivel', required=False)
-    preco_esperado = fields.Float('preco_esperado',required=True)
-    preco_venda = fields.Float('Preco_venda', required=True)
-    quartos = fields.Integer('quartos', required=True)
-    area_estar = fields.Integer('area_estar', required=True)
+    def validade_padrao(self):
+        return date.today() + timedelta(days=90)
+    
+    nome = fields.Char(string='Nome', required=True, translate=True)
+    descricao = fields.Char(string='Descricao', required=False)
+    codigo_postal = fields.Char(string='codigo_postal', required=False)
+    data_disponivel = fields.Date(string='data_disponivel',copy=False, default=validade_padrao)
+    preco_esperado = fields.Float(string='preco_esperado',required=True)
+    preco_venda = fields.Float(string='Preco_venda',readonly=True, copy=False,required=True)
+    quartos = fields.Integer(string="quartos", default=2,required=True)
+    area_estar = fields.Integer(string='area_estar', required=True)
     fachadas = fields.Integer('fachadas', required=True)
     garagem = fields.Boolean('garagem', required=False)
     jardim = fields.Boolean('jardim', required=False)
@@ -21,5 +24,17 @@ class TestModel(models.Model):
     jardim_orientacao = fields.Selection(string='orientacao',
                                          selection=[('norte', 'norte'), ('sul', 'sul')],
                                          help='usado para escolher orientacao')
-
+    state = fields.Selection(
+    selection=[
+        ('new', 'Novo'),
+        ('offer_received', 'Oferta Recebida'),
+        ('offer_accepted', 'Oferta Aceita'),
+        ('sold', 'Vendido'),
+        ('canceled', 'Cancelado')
+    ],
+    string="Status",
+    required=True,
+    copy=False,
+    default='new')
+    active = fields.Boolean(string="Ativo", default=True)
 
